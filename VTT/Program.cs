70ff229fcc16b5c27dacfa0018d6d9c4b9323aa1
MohammadAyaad <155004022+MohammadAyaad@users.noreply.github.com>
2024-11-05 10:04:01 +0200

    feat: added the main function

--- VTT/Program.cs
+++ VTT/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using CliFx;
+
+namespace VTT;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        await new CliApplicationBuilder()
+            //.AddCommand<ICommand>()
+            .Build()
+            .RunAsync(args);
+    }
+}
