07ef9332a824b99fd577d53237bde1fb4397c2f6
MohammadAyaad <155004022+MohammadAyaad@users.noreply.github.com>
2024-12-02 07:28:04 +0200

    init cli

---
+++ VTT/Commands/Test/TestCommand.cs
@@ -0,0 +1,49 @@
+﻿using CliFx;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VersionTaskTracker.Model.Tracker;
+
+namespace VTT.Commands.Test
+{
+    [Command("test")]
+    public class TestCommand : ICommand
+    {
+        public ValueTask ExecuteAsync(IConsole console)
+        {
+            console.Output.WriteLine($"initializing directory...");
+            string currentPath = Directory.GetCurrentDirectory();
+            console.Output.WriteLine($"CURRENT PATH : \'{currentPath}\'");
+
+            Program.Instance.Metadata.RootComponentId = Program.Instance.TasksDbContext.FromDirectory(currentPath,
+                new List<string>()
+                {
+                "node_modules/",
+                ".git/",
+                "src/res/",
+                ".angular/",
+                ".github/",
+                ".vs/",
+                ".vscode/",
+                "dist/",
+                ".gitignore",
+                ".prettierignore",
+                });
+
+            Program.Instance.Save();
+
+            console.Output.WriteLine("RESULT OBJECT : ");
+            console.Output.WriteLine(JObject.FromObject(Program.Instance.TasksDbContext.Components.First(c => c.Id.Equals(Program.Instance.Metadata.RootComponentId))).ToString());
+
+
+
+
+            return ValueTask.CompletedTask;
+        }
+    }
+}
--- VTT/Program.cs
+++ VTT/Program.cs
@@ -1,14 +1,63 @@
-﻿using CliFx;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using CliFx;
+using CliFx.Infrastructure;
+using PathLib;
+using UtilitiesX;
+using UtilitiesX.Extensions;
+using VersionTaskTracker.Services;
+using VTT.Commands.Instance;
+using VTT.Commands.Test;
+using VTT.Commands.VTT;
 
 namespace VTT;
 
 public static class Program
 {
+    public static readonly string ENVIRONMENT_PATH = Path.GetDirectoryName(
+        Assembly.GetExecutingAssembly().Location
+    );
+
+    public static VTTEnvironment Environment;
+    public static VTTInstance? Instance;
+
     public static async Task Main(string[] args)
     {
+        Environment = VTTEnvironment.Setup(ENVIRONMENT_PATH);
+        Instance = new VTTInstance(Directory.GetCurrentDirectory(), Environment.Config);
+
         await new CliApplicationBuilder()
-            //.AddCommand<ICommand>()
+            .AddCommand<VersionCommand>()
+            .AddCommand<TestCommand>()
+            .AddCommand<InitializeInstanceCommand>()
+            .AddCommand<LogCommand>()
+            .AddCommand<StatusCommand>()
+            .AddCommand<AddCommand>()
+            .AddCommand<TrackCommand>()
             .Build()
             .RunAsync(args);
+    }
+
+    private static ValueTask InstanceNotReady(IConsole console)
+    {
+        console.Output.WriteLine("Failed to read instance");
+        return ValueTask.CompletedTask;
+    }
+
+    public static ValueTask WhenInstanceReady(
+        IConsole console,
+        Func<IConsole, VTTInstance, ValueTask> f,
+        Func<IConsole, ValueTask> notReady = null
+    )
+    {
+        notReady ??= InstanceNotReady;
+        if (Instance == null || !Instance.InstanceExists())
+        {
+            return notReady(console);
+        }
+        else
+        {
+            return f(console, Instance);
+        }
     }
 }
