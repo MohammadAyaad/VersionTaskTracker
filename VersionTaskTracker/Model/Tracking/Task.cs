d52d12505fd8cab68a8b0283fb642743b80123a1
MohammadAyaad <mohammadayaad256@gmail.com>
2024-12-07 11:39:47 +0200

    fix: changed the type of Int_Id to be long to allow for larger number of tasks

--- VersionTaskTracker/Model/Tracking/Task.cs
+++ VersionTaskTracker/Model/Tracking/Task.cs
@@ -1,9 +1,9 @@
-﻿namespace VersionTaskTracker.Model.Tracking;
+namespace VersionTaskTracker.Model.Tracking;
 
 public class Task
 {
     public Guid Id { get; set; }
-    public int Int_Id { get; set; }
+    public long Int_Id { get; set; }
     public required string Label { get; set; }
     public required string Status { get; set; }
     public string Description { get; set; } = string.Empty;
